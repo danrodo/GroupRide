10a56d2a6c48d22067e68b2fe3198cbd72e019f6
Daniel Rodosky <djrodosky@gmail.com>
2017-10-22 17:22:54 -0600

    Initial Commit

---
+++ GroupRide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroupRide
+//
+//  Created by Daniel Rodosky on 10/22/17.
+//  Copyright © 2017 Daniel Rodosky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
